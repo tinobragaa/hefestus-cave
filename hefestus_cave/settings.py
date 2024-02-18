acd0b4754871b30fd1e1d6a239f5572baa19d86a
Valentino Braga <valentinobragaa@gmail.com>
2024-02-18 02:17:44 +0000

    Add: included heroku to allowed hosts

--- hefestus_cave/settings.py
+++ hefestus_cave/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%83+n0i@_c&exy)z=i&v17og2_fak)ecy(g9v9*j4rs^zmq(sy'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tinobragaa-hefestuscave-gaa8mr502xz.ws-eu108.gitpod.io',]
+ALLOWED_HOSTS = [
+    'https://hefestus-cave-bb2dd02aee20.herokuapp.com/',
+    'localhost',
+    '8000-tinobragaa-hefestuscave-gaa8mr502xz.ws-eu108.gitpod.io',
+]
 
 
 # Application definition
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hefestus_cave',
 ]
 
 MIDDLEWARE = [
@@ -71,14 +79,19 @@ WSGI_APPLICATION = 'hefestus_cave.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
