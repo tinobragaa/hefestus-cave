734c05f5672076a622602d42301c5481228d5249
Valentino Braga <valentinobragaa@gmail.com>
2024-03-10 21:52:51 +0000

    Fix: removed the year field from the product model

--- products/models.py
+++ products/models.py
@@ -12,7 +12,6 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    year = models.IntegerField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
