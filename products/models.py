cc18670593fe1ef7cb2d4bfe71039541a00c1e7c
Valentino Braga <valentinobragaa@gmail.com>
2024-03-10 21:21:53 +0000

    Add: created products model and migrate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    year = models.IntegerField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1054, null=True, blank=True)
+    stock = models.IntegerField(default=10)
+
+
+    def __str__(self):
+        return self.name
