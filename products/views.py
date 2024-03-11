73adc3a9621d746445e5c2c3fcd8720d3ca72314
Valentino Braga <valentinobragaa@gmail.com>
2024-03-11 05:15:58 +0000

    Add: created product_detail view

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
-    """ A view to show all products."""
+    """ 
+    A view to show all products.
+    """
 
     products = Product.objects.all()
 
@@ -11,3 +13,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to show individual product detail pages.
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
