341d1f34cc254dd8bcba4e6c89b50840ed6d7602
Valentino Braga <valentinobragaa@gmail.com>
2024-03-11 04:12:41 +0000

    Add: created  verbose_name_plural for category to fix admin spelling

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=254)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
