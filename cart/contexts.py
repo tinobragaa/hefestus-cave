0bb99066822129a18d722c8644062237014ad061
Valentino Braga <valentinobragaa@gmail.com>
2024-03-12 05:54:47 +0000

    Add: added for loop to add products to the cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,25 +1,24 @@
-def cart_contents(request):
-
-    cart_items = []
-    total = 0
-    product_count = 0
-
-    context = {
-        'cart_items': cart_items,
-        'total': total,
-        'product_count': product_count,
-    }
-
-    return context
-
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
 
     if total < settings.FREE_SHIPPING_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
