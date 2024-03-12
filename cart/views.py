d99d609efec7c469918d6f32bc40d525b16d378c
Valentino Braga <valentinobragaa@gmail.com>
2024-03-12 03:58:19 +0000

    Add: created the view_cart view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def view_cart(request):
+    """
+    A view to return the cart content page.
+    """
+
+    return render(request, 'cart/cart.html')
