bd25492e8ef7f422c20f3bb66ecf4ae84ffa137e
Valentino Braga <valentinobragaa@gmail.com>
2024-03-12 07:11:32 +0000

    Add: created the adjust_cart view

--- cart/views.py
+++ cart/views.py
@@ -12,6 +12,7 @@ def add_to_cart(request, item_id):
     """
     Add quantity of product to cart.
     """
+    
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -23,3 +24,21 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """
+    Adjust the quantity of a product on the cart page.
+    """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
