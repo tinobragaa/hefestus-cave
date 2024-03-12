6fed2d20583a887254fe3f1d5ab6ab1ed0c58e00
Valentino Braga <valentinobragaa@gmail.com>
2024-03-12 07:32:05 +0000

    Add: created remove_from_cart view

--- cart/views.py
+++ cart/views.py
@@ -43,3 +43,20 @@ def adjust_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+def remove_from_cart(request, item_id):
+    """
+    Remove a product from the cart.
+    """
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+    
