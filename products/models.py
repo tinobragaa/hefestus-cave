ca668758b7229f663b4feabcda9026e948cc8c8f
Valentino Braga <valentinobragaa@gmail.com>
2024-03-12 03:04:06 +0000

    Add: created display_name to the category view

--- products/models.py
+++ products/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
+    
+    def get_display_name(self):
+        return self.display_name
 
 
 class Product(models.Model):
