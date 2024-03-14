fb2d3b23fb055e5609970dc9cc6dd5b7a8567f62
Valentino Braga <valentinobragaa@gmail.com>
2024-03-14 01:34:16 +0000

    Add: created add_product view

--- products/models.py
+++ products/models.py
@@ -15,7 +15,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     sku = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
