d894d678412d37930dddee183cf165284bb3ad4b
Valentino Braga <valentinobragaa@gmail.com>
2024-03-14 06:31:56 +0000

    Add: created handler500 function

--- hefestus_cave/views.py
+++ hefestus_cave/views.py
@@ -7,3 +7,11 @@ def handler404(request, exception):
     """
 
     return render(request, "errors/404.html", status=404)
+
+
+def handler500(request):
+    """
+    Error Handler 500 - Internal Server Error.
+    """
+
+    return render(request, "errors/500.html", status=500)
