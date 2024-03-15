abb633710817459bc0b4d6779d97cc55504dba15
Valentino Braga <valentinobragaa@gmail.com>
2024-03-15 12:07:39 +0000

    Add: created the contact view

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def contact(request):
+    """
+    A view to return the contact page.
+    """
+    form = ContactForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'contact/contact.html', context)
