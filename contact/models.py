5a5a96a682e655882978b3a20a10f3e55f119564
Valentino Braga <valentinobragaa@gmail.com>
2024-03-16 04:54:51 +0000

    Add: created the contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Created when a user submits a form on the contact page.
+    """
+
+    name = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    contact_reason = models.CharField(max_length=100)
+    message = models.TextField(null=False, blank=False)
+    date_submmited = models.DateTimeField(default=timezone.now)
