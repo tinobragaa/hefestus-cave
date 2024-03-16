8ab630b2dc06fa471644bc7bcdf388d640136e33
Valentino Braga <valentinobragaa@gmail.com>
2024-03-16 05:34:25 +0000

    Add: added placeholder to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -8,6 +8,21 @@ class ContactForm(forms.ModelForm):
     the contact form.
     """
     
+    # Define placeholders as class variables
+    placeholders = {
+        'name': 'Name',
+        'email': 'Email',
+        'contact_reason': 'Order Number or Contact Reason',
+    }
+    
     class Meta:
         model = Contact
         fields = ('name', 'email', 'contact_reason', 'message',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Loop through placeholders defined in the class and set them as placeholders
+        for field_name, placeholder_text in self.placeholders.items():
+            if field_name in self.fields:
+                self.fields[field_name].widget.attrs['placeholder'] = placeholder_text
